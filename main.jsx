50ade3175c633a6390d81e951a0c6ed3325113c7
Jeremiah <je901762@ucf.edu>
2026-03-08 15:16:26 -0400

    Main.jsx
    updated the direction of the jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import CinaMaxReview from './CinaMaxReview.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CinaMaxReview />
   </React.StrictMode>
 )
